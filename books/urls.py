56bfd9a4ae2364b32dbe526cc793a5c9afd98da2
chukhraiartur <chukhraiartur@gmail.com>
2022-09-01 00:58:23 +0300

    refactor(views.py): changed 'view functions' to 'view classes'

--- books/urls.py
+++ books/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),                                       # http://127.0.0.1:8000/
+    path('', BooksHome.as_view(), name='home'),                         # http://127.0.0.1:8000/
     path('about/', about, name='about'),                                # http://127.0.0.1:8000/about/
-    path('addbook/', add_book, name='add_book'),                        # http://127.0.0.1:8000/addbook/
+    path('addbook/', AddBook.as_view(), name='add_book'),                        # http://127.0.0.1:8000/addbook/
     path('contact/', contact, name='contact'),                          # http://127.0.0.1:8000/contact/
     path('login/', login, name='login'),                                # http://127.0.0.1:8000/login/
-    path('book/<slug:book_slug>/', show_book, name='show_book'),        # http://127.0.0.1:8000/books/1/
-    path('category/<slug:cat_slug>/', show_category, name='category'),  # http://127.0.0.1:8000/category/1/
+    path('book/<slug:book_slug>/', ShowBook.as_view(), name='show_book'),        # http://127.0.0.1:8000/books/1/
+    path('category/<slug:cat_slug>/', BooksCategory.as_view(), name='category'),  # http://127.0.0.1:8000/category/1/
     
     
     
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
-import pkgutil
+# import pkgutil
+from django.views.generic import ListView, DetailView, CreateView
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
@@ -13,39 +14,65 @@ menu = [
 ]
 
 # Create your views here.
-def index(request):
-    # books = Books.objects.all()
-    books = Books.objects.order_by('-time_create')
 
-    context = {
-        'books': books,
-        'menu': menu,
-        'title': 'Main page',
-        'cat_selected': 0,
-    }
+class BooksHome(ListView):
+    model = Books
+    template_name = 'books/index.html'
+    context_object_name = 'books'
 
-    return render(request, 'books/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main page'
+        context['menu'] = menu
+        context['cat_selected'] = 0
+        return context
+    
+    def get_queryset(self):
+        return Books.objects.filter(is_published=True).order_by('-time_create')
+
+# def index(request):
+#     # books = Books.objects.all()
+#     books = Books.objects.order_by('-time_create')
+
+#     context = {
+#         'books': books,
+#         'menu': menu,
+#         'title': 'Main page',
+#         'cat_selected': 0,
+#     }
+
+#     return render(request, 'books/index.html', context=context)
 
 def about(request):
     return render(request, 'books/about.html', {'menu': menu, 'title': 'About the site'})
 
-def add_book(request):
-    if request.method == 'POST':
-        form = AddBookForm(request.POST, request.FILES)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            form.save()
-            return redirect('home')       
-    else:
-        form = AddBookForm()
+class AddBook(CreateView):
+    form_class = AddBookForm
+    template_name = 'books/add_book.html'
 
-    context = {
-        'form': form, 
-        'menu': menu, 
-        'title': 'Add a book that you like and want to share with other people 😊'
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add book'
+        context['menu'] = menu
+        return context
 
-    return render(request, 'books/add_book.html', context=context)
+# def add_book(request):
+#     if request.method == 'POST':
+#         form = AddBookForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')       
+#     else:
+#         form = AddBookForm()
+
+#     context = {
+#         'form': form, 
+#         'menu': menu, 
+#         'title': 'Add a book that you like and want to share with other people 😊'
+#     }
+
+#     return render(request, 'books/add_book.html', context=context)
 
 def contact(request):
     return HttpResponse('Обратная связь')
@@ -53,34 +80,65 @@ def contact(request):
 def login(request):
     return HttpResponse('Авторизация')
 
-def show_book(request, book_slug):
-    book = get_object_or_404(Books, slug=book_slug)
+class ShowBook(DetailView):
+    model = Books
+    template_name = 'books/book.html'
+    context_object_name = 'book'
+    slug_url_kwarg = 'book_slug'                 # user slug name in the urls
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['book']
+        context['menu'] = menu
+        # context['cat_selected'] = Category.objects.get(slug=self.kwargs['cat_slug']).id
+        context['cat_selected'] = context['book'].cat_id
+        return context
 
-    context = {
-        'book': book,
-        'menu': menu,
-        'title': book.title,
-        'cat_selected': book.cat_id,
-    }
+# def show_book(request, book_slug):
+#     book = get_object_or_404(Books, slug=book_slug)
 
-    return render(request, 'books/book.html', context=context)
+#     context = {
+#         'book': book,
+#         'menu': menu,
+#         'title': book.title,
+#         'cat_selected': book.cat_id,
+#     }
+
+#     return render(request, 'books/book.html', context=context)
 
 
-def show_category(request, cat_slug):
-    cat = Category.objects.get(slug=cat_slug)
-    books = Books.objects.filter(cat_id=cat.id)
+class BooksCategory(ListView):
+    model = Books
+    template_name = 'books/index.html'
+    context_object_name = 'books'
+    allow_empty = False                 # if slug is incorrect show 404 error
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Display by ' + str(context['books'][0].cat)
+        context['menu'] = menu
+        # context['cat_selected'] = Category.objects.get(slug=self.kwargs['cat_slug']).id
+        context['cat_selected'] = context['books'][0].cat_id
+        return context
 
-    if len(books) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Books.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'books': books,
-        'menu': menu,
-        'title': 'Display by category',
-        'cat_selected': cat.id,
-    }
+# def show_category(request, cat_slug):
+#     cat = Category.objects.get(slug=cat_slug)
+#     books = Books.objects.filter(cat_id=cat.id)
 
-    return render(request, 'books/index.html', context=context)
+#     if len(books) == 0:
+#         raise Http404()
+
+#     context = {
+#         'books': books,
+#         'menu': menu,
+#         'title': 'Display by category',
+#         'cat_selected': cat.id,
+#     }
+
+#     return render(request, 'books/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound(f"<h1>Oops...</h1>")
