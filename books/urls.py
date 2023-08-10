05cb9d15696f66a57b307b9ddb1387aa83b8925e
chukhraiartur <chukhraiartur@gmail.com>
2023-08-10 14:33:32 +0300

    update: added new routes

--- books/urls.py
+++ books/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 urlpatterns = [
-    path('', BooksHome.as_view(), name='home'),                         # http://127.0.0.1:8000/
-    path('about/', about, name='about'),                                # http://127.0.0.1:8000/about/
-    path('addbook/', AddBook.as_view(), name='add_book'),                        # http://127.0.0.1:8000/addbook/
-    path('contact/', contact, name='contact'),                          # http://127.0.0.1:8000/contact/
-    path('login/', login, name='login'),                                # http://127.0.0.1:8000/login/
-    path('book/<slug:book_slug>/', ShowBook.as_view(), name='show_book'),        # http://127.0.0.1:8000/books/1/
-    path('category/<slug:cat_slug>/', BooksCategory.as_view(), name='category'),  # http://127.0.0.1:8000/category/1/
-    
-    
-    
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)               # http://127.0.0.1:8000/archive/2022/
+    path('', BooksHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addbook/', AddBook.as_view(), name='add_book'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('sign_up/', SignUpUser.as_view(), name='sign_up'),
+    path('sign_in/', SignInUser.as_view(), name='sign_in'),
+    path('sign_out/', sign_out_user, name='sign_out'),
+    path('book/<slug:book_slug>/', ShowBook.as_view(), name='show_book'),
+    path('category/<slug:cat_slug>/', BooksCategory.as_view(), name='category'),
 ]
