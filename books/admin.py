65aebbf3690ded9f331fa0e75c98d0a8c6517215
chukhraiartur <chukhraiartur@gmail.com>
2023-08-10 12:01:00 +0300

    update(admin): configured admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
-# Register your models here.
+
 class BooksAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'author', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'author')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'author', 'description', 'photo', 'get_html_photo', 'is_published', 'cat', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    # the function specifies not to escape the content (so that the image is displayed)
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Thumbnail'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -16,5 +29,9 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name', )
     prepopulated_fields = {'slug': ('name',)}
 
+
 admin.site.register(Books, BooksAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_header = 'Admin panel of the website about books'
+admin.site.site_title = 'Admin panel of the website about books'
